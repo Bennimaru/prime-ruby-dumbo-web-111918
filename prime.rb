7f3aea6d0f3dec2592e43bfa36681126fdc84f49
Benson Yu <bennywhyu@gmail.com>
2018-11-13 20:48:03 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,12 @@
 # Add  code here!
 def prime?(n)
-  if n<1
-  
-  
-  (2..n-1).none? {|x|
+  if n<=1 
+    return false
+  elsif n%2==0 
+    return false
+  else
+    (2..n-1).none? {|x|
     n%x == 0
-    if n<0 
-      return false
-    end
-  }
+    }
+  end
 end
